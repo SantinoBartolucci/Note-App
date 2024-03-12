0023f3496a6864e90d5a32bc3f04bba3375663d4
/Santino <74921908+SantinoBartolucci@users.noreply.github.com>
2024-03-12 17:14:36 -0300

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
 	mongodb: {
-		URI: 'mongodb+srv://Santino:xKUa33oeHmh5kB9C@cluster0.adsh6ab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+		URI: 'mongodb+srv://<usuario>:<contraseña>@cluster0.adsh6ab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
 	},
 };
